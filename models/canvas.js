180c2b8ce0c6e8486b61b5d1c0f7f740f04afaf2
penguoir <games@marash.net>
2017-11-20 14:06:45 +0000

    Update canvas.js

--- models/canvas.js
+++ models/canvas.js
@@ -3,7 +3,7 @@ var crypto = require('crypto')
 var Canvas = new OAuth2({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.SECRET,
-  state: crypto.randomBytes(16).toString('hex'),
+  state: '1234',
   authorizationUri: `https://kingalfred.test.instructure.com/login/oauth2/auth`,
   redirectUri: 'https://canvashodreports.herokuapp.com/login/callback',
   accessTokenUri: `https://kingalfred.test.instructure.com/login/oauth2/token`
